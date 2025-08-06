03eefa872bbfafab0a56644a4409cd18a06f6678
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-06 13:24:32 +0530

    Update App.jsx

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <nav>
+      <img src="/public/images/nav-logo.svg" alt="" className="scale-90" />
+      <img src="/public/images/menu.svg" alt="" className="w-10" />
+    </nav>
+  );
+}
+
+export default NavBar;
